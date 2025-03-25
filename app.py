8b404fef28deda438f972cd69ee468617c9f83e4
Emmanuel Acheampong <achampion.emma@gmail.com>
2025-03-25 14:22:39 -0400

    V1
    Pushing a simple classifier first

---
+++ app.py
@@ -0,0 +1,52 @@
+
+import keras
+import numpy as np
+from keras.applications import vgg16
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.imagenet_utils import decode_predictions
+from PIL import Image
+import requests
+from io import BytesIO
+import streamlit as st
+
+
+vgg_model = vgg16.VGG16(weights='imagenet')
+
+def predict_image(image_path, model):
+    original = Image.open(image_path)
+
+    newsize = (224, 224) 
+    original = original.resize(newsize) 
+
+    numpy_image = img_to_array(original)
+
+    image_batch = np.expand_dims(numpy_image, axis=0)
+
+    # prepare the image for the VGG model
+    processed_image = vgg16.preprocess_input(image_batch.copy())
+
+    # get the predicted probabilities for each class
+    predictions = model.predict(processed_image)
+
+    label = decode_predictions(predictions)
+    if label[0][0][-1] >= 0.5:
+        return(label[0][0]) #just display top 2
+
+image = Image.open('./img/1.jpg')
+
+st.header("Perfume Classifier")
+
+st.image(image)
+
+st.body("Classification Model for Perfume")
+
+uploaded_file = st.file_uploader("Choose an image file", type="jpg")
+
+if uploaded_file:
+    predicted = predict_image(uploaded_file, vgg_model)
+    st.image(uploaded_file)
+
+    st.text("Classification: " + predicted[0][0][1])
+    st.text("Accuracy: " + predicted[0][0][-1])
